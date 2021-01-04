58aa968183d89d1d4888a27393808ceb60be355c
rishi-live <rishi.uemk9@gmail.com>
2021-01-04 12:02:29 +0530

    Destructuring and property shorthand change

--- app.js
+++ app.js
@@ -27,19 +27,19 @@ const address = process.argv[2];
 if (!address) {
     console.log("Please provide a valid address");
 } else {
-    geocode(address, (error, data) => {
+    geocode(address, (error, { latitude, longitude, location } = {}) => {
         if (error) {
             return console.log(error);
         }
         // console.log('Error = ', error);
         // console.log('Data = ', data);
 
-        forecast(data.latitude, data.longitude, (error, forecastData) => {
+        forecast(latitude, longitude, (error, forecastData) => {
             if (error) {
                 return console.log(error);
             }
 
-            console.log(data.location);
+            console.log(location);
             console.log(forecastData);
 
             // console.log('Error = ', error);
--- utils/forecast.js
+++ utils/forecast.js
@@ -2,14 +2,14 @@ const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=9ace7eed66082bb877bdaf211241475b&query=' + latitude + ',' + longitude + '&units=f';
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect', undefined);
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location', undefined);
         } else {
             //console.log();
-            callback(undefined, response.body.current.weather_descriptions);
+            callback(undefined, body.current.weather_descriptions);
         }
     })
 
